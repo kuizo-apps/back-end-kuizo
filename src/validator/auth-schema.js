0ae1c48e8be8d8007752bd60e5b07e5a80462c9b
mhmadilham18 <f1d022061@student.unram.ac.id>
2025-11-23 16:22:55 +0800

    try

--- src/validator/auth-schema.js
+++ src/validator/auth-schema.js
@@ -9,14 +9,14 @@ export const RegisterAdminSchema = Joi.object({
 });
 
 export const RegisterUserSchema = Joi.object({
-  username: Joi.string().min(3).max(50).required(),
-  full_name: Joi.string().min(3).max(100).required(),
+  username: Joi.string().required(),
+  full_name: Joi.string().required(),
   email: Joi.string().email().required(),
-  nomer_induk: Joi.string().min(5).max(20).required(),
-  password: Joi.string().min(6).required(),
+  nomer_induk: Joi.string().required(),
+  password: Joi.string().required(),
   role: Joi.string().valid("guru", "siswa").required(),
-  class_student: Joi.string().max(8).allow(null),
-});
+  class_student: Joi.string().allow(null, ""),
+}).unknown(true);
 
 export const LoginSchema = Joi.object({
   email: Joi.string().email().required(),
